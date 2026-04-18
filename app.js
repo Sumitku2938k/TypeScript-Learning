e9402fb94d26e2dd17aacbc09221872187f4df92
Sumit Kumar <sumitku2938k@gmail.com>
2026-04-18 20:44:56 +0530

    Introduction to TypeScript basic types and reference types.

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 "use strict";
+// Basic Types
+// Primitives: number, string, boolean, null, undefined, symbol, bigint
+// Arrays
+// Tuples
+// Enums
+// Any, Unknown, Never, Void, Null, Undefined
 Object.defineProperty(exports, "__esModule", { value: true });
-var a = 12;
-function add(x, y) {
-    return x + y;
-}
+let num = 42;
+let str = "Hello, TypeScript!";
+// Primitives and Reference Types
+let a = [1, 2, 3, 4, 5];
+let b = a;
+b.pop();
+console.log(a);
+// b ke aakhiri se no hata do i.e. b = [1,2,3,4]
+// so a ki value ye honi chahiye thi [1,2,3,4,5] but wo bhi change ho jayegi i.e. a = [1,2,3,4] 
+// because a and b dono same reference point kar rahe hai memory me. isliye jab b me change karenge to a me bhi change hoga. isliye reference types ko samajhna bahut jaruri hai. 
+// Reference Types -> {}, [], () etc.
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,5 +1,20 @@
-var a = 12;
+// Basic Types
+// Primitives: number, string, boolean, null, undefined, symbol, bigint
+// Arrays
+// Tuples
+// Enums
+// Any, Unknown, Never, Void, Null, Undefined
 
-function add(x: number, y: number): number {
-    return x + y;
-}
+let num: number = 42;
+let str: string = "Hello, TypeScript!";
+
+// Primitives and Reference Types
+let a: number[] = [1,2,3,4,5];
+let b: number[] = a;
+b.pop();
+console.log(a);
+// b ke aakhiri se no hata do i.e. b = [1,2,3,4]
+// so a ki value ye honi chahiye thi [1,2,3,4,5] but wo bhi change ho jayegi i.e. a = [1,2,3,4] 
+// because a and b dono same reference point kar rahe hai memory me. isliye jab b me change karenge to a me bhi change hoga. isliye reference types ko samajhna bahut jaruri hai. 
+
+// Reference Types -> {}, [], () etc.
