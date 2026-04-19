3f0818bcd60c29969289da4bc80318b39056885c
Sumit Kumar <sumitku2938k@gmail.com>
2026-04-19 21:17:57 +0530

    Arrays in TypeScript

--- app.js
+++ app.js
@@ -18,7 +18,12 @@ let str = "Hello, TypeScript!";
 // because a and b dono same reference point kar rahe hai memory me. isliye jab b me change karenge to a me bhi change hoga. isliye reference types ko samajhna bahut jaruri hai. 
 // Reference Types -> {}, [], () etc.
 //Primitive Types
-let a = 12;
-a = 'Hello'; // Error: Type 'string' is not assignable to type 'number'.
-let abcd = false; //boolean type -> true or false
+// let a = 12;
+// a = 'Hello'; // Error: Type 'string' is not assignable to type 'number'.
+// let abcd = false; //boolean type -> true or false
+// Arrays
+let arr = [1, 2, 3, 4, 5, "Goutam"]; // TypeScript infers the type as (number | string)[]
+let arr2 = [1, 2, 3, 4, 5, { name: "Goutam" }, { name: 12 }]; // TypeScript infers the type as (number | {name: string} | {name: number}[]
+let arr3 = [1, 2, 3, 4, 5, "Goutam"]; // Explicitly specifying the type as number[] so will give an error because "Goutam" is a string and cannot be assigned to a number type array.
+let arr4 = [1, 2, 3, 4, 5, "Goutam"]; // Explicitly specifying the type as (number | string)[]
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -21,7 +21,14 @@ let str: string = "Hello, TypeScript!";
 
 
 //Primitive Types
-let a = 12;
-a = 'Hello'; // Error: Type 'string' is not assignable to type 'number'.
+// let a = 12;
+// a = 'Hello'; // Error: Type 'string' is not assignable to type 'number'.
 
-let abcd = false; //boolean type -> true or false
+// let abcd = false; //boolean type -> true or false
+
+// Arrays
+let arr = [1, 2, 3, 4, 5, "Goutam"]; // TypeScript infers the type as (number | string)[]
+let arr2 = [1, 2, 3, 4, 5, {name: "Goutam"}, {name: 12}]; // TypeScript infers the type as (number | {name: string} | {name: number}[]
+
+let arr3: number[] = [1, 2, 3, 4, 5, "Goutam"]; // Explicitly specifying the type as number[] so will give an error because "Goutam" is a string and cannot be assigned to a number type array.
+let arr4: (number | string)[] = [1, 2, 3, 4, 5, "Goutam"]; // Explicitly specifying the type as (number | string)[]
