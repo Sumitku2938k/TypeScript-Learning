c7861d36afadf333dfd134d242a241b8d6b68eed
Sumit Kumar <sumitku2938k@gmail.com>
2026-04-23 20:52:44 +0530

    Classes and Objects: this.keyword

--- app.js
+++ app.js
@@ -1,53 +1,22 @@
 "use strict";
-// Classes and Objects: Constructor
-// Object.defineProperty(exports, "__esModule", { value: true });
-// ek esi machine jo ki produce kr rhi hai final consumable product -> Constructor
-// class BottleMaker {
-//     constructor (public name: string, public price: number) {}
-// }
-// let b1 = new BottleMaker("Milton",1200);
-class HumanMaker {
-    name;
-    gender;
-    age = 0; // default value i.e. Jo baat hum pehle se jante hai ki har human ki age 0 se start hoti hai, toh hum usko yaha pe define kr sakte hai. Aur isko constructor ke andar define krne ki zarurat nahi hai.
-    constructor(name, gender) {
-        this.name = name;
-        this.gender = gender;
+Object.defineProperty(exports, "__esModule", { value: true });
+//Classes and Objects: this.keyword
+class Abcd {
+    name = "Goutam";
+    changeSomeStuff() {
+        console.log(this.name);
+        this.changeSomeMoreStuff(); //kisi bhi function ya variable jo bhi class ke andar hoga usse use/access karne ke liye this.keyword ka use karte hai
+    }
+    changeSomeMoreStuff() {
+        console.log("Hey");
     }
 }
-new HumanMaker("Goutam", "Male"); //Constructor sirf wo mang raha hai joo hame set karna hai
-class Music {
+class BottleMaker {
     name;
-    artist;
-    thumbnail;
-    length;
-    available;
-    constructor(name, artist, thumbnail = "somethumbnail.jpg", length, available) {
+    constructor(name) {
         this.name = name;
-        this.artist = artist;
-        this.thumbnail = thumbnail;
-        this.length = length;
-        this.available = available;
-        if (!thumbnail) {
-            this.thumbnail = "somethumbnail.jpg";
-        }
+        this.name = name; //this.name is variable of class and name is parameter of constructor
     }
 }
-let m1 = new Music("Chal Chaiya chhaiya", "Goutam", "", 1200, false);
-let m2 = new Music("Dekh Dekh Dekh Yaha Waha Na Fekh, Dekh Failegi Beemari Hoga Sabka Bura Haal", "Nagar Paalika", "Kachra.png", 2000, true);
-//Same kam kar raha hai but lines of code bohot jayada hai as compared to pehle wale tarike mei
-// class Music {
-//     public name;
-//     public artist;
-//     public thumbnail;
-//     public length;
-//     public available;
-//     constructor ( name: string, artist: string, thumbnail: string = "somethumbnail.jpg", length: number, available: boolean){
-//         this.name = name;
-//         this.artist = artist;
-//         this.thumbnail = thumbnail;
-//         this.length = length;
-//         this.available = available;
-//     }
-// }
+let b1 = new BottleMaker("Milton"); //Milton gya hai name parameter mei
 //# sourceMappingURL=app.js.map
