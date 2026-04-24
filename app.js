be49a6628fb68fe3b5f8508a0d427f387cbdec7e
Sumit Kumar <sumitku2938k@gmail.com>
2026-04-24 20:06:22 +0530

    Classes & Objects: Public & Private Access Modifier

--- app.js
+++ app.js
@@ -1,22 +1,50 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-//Classes and Objects: this.keyword
-class Abcd {
-    name = "Goutam";
-    changeSomeStuff() {
-        console.log(this.name);
-        this.changeSomeMoreStuff(); //kisi bhi function ya variable jo bhi class ke andar hoga usse use/access karne ke liye this.keyword ka use karte hai
-    }
-    changeSomeMoreStuff() {
-        console.log("Hey");
-    }
-}
+// Classes & Objects: Public & Private Access Modifier
 class BottleMaker {
     name;
-    constructor(name) {
+    material;
+    constructor(name, material) {
         this.name = name;
-        this.name = name; //this.name is variable of class and name is parameter of constructor
+        this.material = material;
+    }
+    changing() {
+        this.material = "Plastic";
     }
 }
-let b1 = new BottleMaker("Milton"); //Milton gya hai name parameter mei
+let b1 = new BottleMaker("Milton", "Metal");
+b1.name = "hululu"; //public property ki chijo ko class ke andar ya bahar kahi bhi change kar sakte hai  
+// b1.material = "Hul"; //private property ko change kar kar to sakte hai but ye error phir  bhi dikhayega
+b1.changing();
+//Private ka variable sirf class ke andar hi accessible hai iske bahar accessible nahi hai
+// class LunchMaker {
+//     constructor(public name: string){ }
+// }
+// class MetalLunchMaker extends LunchMaker {
+//     constructor(name: string){
+//         super(name);
+//     }
+//     getValue(){
+//         console.log(this.name);
+//     }
+// }
+// let l1 = new MetalLunchMaker("Milton");
+// l1.getValue();
+class LunchMaker {
+    name;
+    halua = "halua";
+    constructor(name) {
+        this.name = name;
+    }
+}
+class MetalLunchMaker extends LunchMaker {
+    constructor(name) {
+        super(name);
+    }
+    getValue() {
+        console.log(this.name, this.halua); //halua is private so using it will show an error but will work
+    }
+}
+let l1 = new MetalLunchMaker("Milton");
+l1.getValue();
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,19 +1,49 @@
-//Classes and Objects: this.keyword
-class Abcd {
-    name = "Goutam";
-
-    changeSomeStuff(){ //Function inside a class -> Method
-        console.log(this.name);
-        this.changeSomeMoreStuff(); //kisi bhi function ya variable jo bhi class ke andar hoga usse use/access karne ke liye this.keyword ka use karte hai
-    }
-    changeSomeMoreStuff(){
-        console.log("Hey");
-    }
-}
-
+// Classes & Objects: Public & Private Access Modifier
 class BottleMaker {
-    constructor(public name: string){ 
-        this.name = name; //this.name is variable of class and name is parameter of constructor
+    constructor(public name: string, private material: string){ 
+
+    }
+    changing(){
+        this.material = "Plastic";
+    }
+
+}
+let b1 = new BottleMaker("Milton", "Metal");
+b1.name = "hululu"; //public property ki chijo ko class ke andar ya bahar kahi bhi change kar sakte hai  
+// b1.material = "Hul"; //private property ko change kar kar to sakte hai but ye error phir  bhi dikhayega
+b1.changing();
+
+//Private ka variable sirf class ke andar hi accessible hai iske bahar accessible nahi hai
+
+// class LunchMaker {
+//     constructor(public name: string){ }
+// }
+
+// class MetalLunchMaker extends LunchMaker {
+//     constructor(name: string){
+//         super(name);
+//     }
+//     getValue(){
+//         console.log(this.name);
+//     }
+// }
+
+// let l1 = new MetalLunchMaker("Milton");
+// l1.getValue();
+
+class LunchMaker {
+    private halua: string = "halua";
+    constructor(public name: string){ }
+}
+
+class MetalLunchMaker extends LunchMaker {
+    constructor(name: string){ 
+        super(name);
+    }
+    getValue(){
+        console.log(this.name, this.halua); //halua is private so using it will show an error but will work
     }
 }
-let b1 = new BottleMaker("Milton"); //Milton gya hai name parameter mei
+
+let l1 = new MetalLunchMaker("Milton");
+l1.getValue();
