91f591f66328848cf2de3073b9b0251c7e489a89
Sumit Kumar <sumitku2938k@gmail.com>
2026-06-16 20:32:31 +0530

    Type Assertion

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-//Modules
-import { addPayment, getDetails } from './payment.js';
-addPayment(100);
-getDetails();
-import bottleMaker from './bottleMaker.js';
-const bottle1 = new bottleMaker('Coca Cola');
-console.log(bottle1.name);
+//Type Assertion
+//Iska mtlb batana TS ko ki particular cheej ka type kya hai, ye aap tab karte ho jab aap TS se jayada us value ka type jaante ho
+//Syntax: <type>value or value as type
+let a = 12;
+a; //You will get all methods that should be in a number type
+let b = "Hello World";
+b; //You will get all methods that should be in a string type
+let c = Number("12"); //Type Casting -> Here converting the type of c which is string to a number
+console.log(typeof c);
+export {};
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,9 +1,21 @@
-//Modules
+//Type Assertion
 
-import { addPayment, getDetails } from './payment.js';
-addPayment(100);
-getDetails();
+//Iska mtlb batana TS ko ki particular cheej ka type kya hai, ye aap tab karte ho jab aap TS se jayada us value ka type jaante ho
 
-import bottleMaker from './bottleMaker.js';
-const bottle1 = new bottleMaker('Coca Cola');
-console.log(bottle1.name);
+//Syntax: <type>value or value as type
+
+let a: any = 12;
+(<number>a) //You will get all methods that should be in a number type
+
+let b: any = "Hello World";
+(b as string) //You will get all methods that should be in a string type
+
+//Type Casting 
+let c = Number("12"); // Here converting the type of c which is string to a number
+console.log(typeof c);
+
+// Non-null assertion operator
+let d: null | undefined | string;
+d = "Hi!";
+
+d! // ! guarantees that this value can never be null or undefined for sure
